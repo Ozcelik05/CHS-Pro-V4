5d64e60d0ff353dbe87a7a4c5fd85957bdf84d43
Your Name <you@example.com>
2025-11-10 01:50:57 +0300

    Fix: Simplify Android project and dependencies

--- android/app/src/main/java/com/ozcelik05/chspro/MainActivity.java
+++ android/app/src/main/java/com/ozcelik05/chspro/MainActivity.java
@@ -1,11 +1,15 @@
-package com.example.chspro;
+package com.ozcelik05.chspro;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Basit bir aktivite - hata vermez
+        TextView textView = new TextView(this);
+        textView.setText("CHS Pro V4 Çalışıyor!");
+        setContentView(textView);
     }
 }
